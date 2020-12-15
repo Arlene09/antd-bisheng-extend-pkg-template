f9451105dcebad7c05c45ec8b4e6b98f4387cf49
shaozhengqing <z0000217@ad.zto.com>
2020-12-15 16:55:51 +0800

    项目初始化配置文件

---
+++ bisheng.config.js
@@ -0,0 +1,91 @@
+const alertLessConfig = (rules) => {
+  rules.forEach((rule) => {
+    if (rule.loader && rule.loader.includes('less-loader')) {
+      rule.options = rules.options || {};
+      rule.options.javascriptEnabled = true;
+    } else if (rule.use) {
+      alertLessConfig(rule.use);
+    }
+  });
+};
+
+const alertCssConfig = (rules) => {
+  rules.forEach((rule) => {
+    if (rule.loader && rule.loader.includes('css-loader')) {
+      rule.options = Object.assign(rules.options || {}, {
+        modules: 'global'
+      });
+      // rule.options.javascriptEnabled = true;
+    } else if (rule.use) {
+      alertCssConfig(rule.use);
+    }
+  });
+};
+
+const resetTsConfig = (rules) => {
+  rules.forEach((rule) => {
+    if (rule.test && rule.test.toString() === /\.tsx?$/.toString()) {
+      rule.use.forEach((tsRule) => {
+        if (tsRule.loader && /ts-loader/.test(tsRule.loader)) {
+          tsRule.loader = require.resolve('ts-loader');
+        }
+      });
+    }
+  });
+};
+
+const filePath = '';
+
+module.exports = {
+  history: 'hash',
+  hash: true,
+  port: 9000,
+  source: {
+    guide: ['./guide'],
+    packages: './packages'
+  },
+  output: './docs',
+  theme: '@doddle/doddle-bisheng-theme',
+  entiryName: 'index',
+  themeConfig: {
+    home: '/guide/introduce',
+    root: filePath,
+    combineChangelog: false,
+    compSorterType: 'native',
+    title: 'react组件库包',
+    friendLinks: [
+      { name: 'antd', link: 'https://ant-design.gitee.io/index-cn' },
+      { name: 'react', link: 'https://react.docschina.org/tutorial/tutorial.html' },
+      { name: 'BiSheng', link: 'https://github.com/benjycui/bisheng' }]
+  },
+  devServerConfig: {},
+  webpackConfig(config) {
+    config.devtool = 'source-map';
+    if (process.env.NODE_ENV === 'production') {
+      config.devtool = 'none';
+      config.mode = 'production';
+      config.optimization = {
+        splitChunks: {
+          minSize: 30000,
+          cacheGroups: {
+            antd: {
+              test: /[\\/]node_modules[\\/]antd[\\/]/,
+              name: 'vendor-antd',
+              chunks: 'all'
+            },
+            vendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom|moment|react-document-title|bind-decorator)[\\/]/,
+              name: 'vendor-common',
+              chunks: 'all'
+            },
+          }
+        }
+      };
+    }
+    alertLessConfig(config.module.rules);
+    alertCssConfig(config.module.rules);
+    resetTsConfig(config.module.rules);
+    return config;
+  },
+  root: `${filePath}/`
+};
